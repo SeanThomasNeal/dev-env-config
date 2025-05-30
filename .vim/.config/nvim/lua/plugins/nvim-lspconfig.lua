39dd5a303f6f7f9e65b3c4cb2015dc4e09b20e36
SeanNeal572 <sean.neal@chillisoft.co.za>
2025-05-30 21:42:21 +0200

    Setup biome formatting

--- .vim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .vim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -11,6 +11,7 @@ return {
       ts_ls = {
         enabled = false,
       },
+      biome = {},
       vtsls = {
         -- explicitly add default filetypes, so that we can extend
         -- them in related extras
