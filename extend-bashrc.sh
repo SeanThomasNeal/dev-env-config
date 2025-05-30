b478b768b881b55ef12ffd9ae5560b2b6c9ea420
SeanNeal572 <sean.neal@chillisoft.co.za>
2025-05-30 11:24:45 +0200

    Vim aliases

--- extend-bashrc.sh
+++ extend-bashrc.sh
@@ -1,12 +1,14 @@
 SETUP_DIR="$DEV_ENV_CONFIG_DIR/setup"
 
 . $SETUP_DIR/nvm.sh
+. $SETUP_DIR/vim.sh
 
 echo "Setup loaded..."
 
 ALIASES_DIR="$DEV_ENV_CONFIG_DIR/aliases"
 
 . $ALIASES_DIR/git.sh
+. $ALIASES_DIR/vim.sh
 
 echo "Aliases loaded..."
 
