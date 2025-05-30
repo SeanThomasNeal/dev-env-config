a570680ad088113cf28d4ced4827013607129431
SeanNeal572 <sean.neal@chillisoft.co.za>
2025-05-30 10:05:08 +0200

    Git aliases

--- extend-bashrc.sh
+++ extend-bashrc.sh
@@ -1,3 +1,13 @@
-. $DEV_ENV_CONFIG_DIR/nvm.sh
+SETUP_DIR="$DEV_ENV_CONFIG_DIR/setup"
+
+. $SETUP_DIR/nvm.sh
+
+echo "Setup loaded..."
+
+ALIASES_DIR="$DEV_ENV_CONFIG_DIR/aliases"
+
+. $ALIASES_DIR/git.sh
+
+echo "Aliases loaded..."
 
 echo "Loaded dev-env-config"
