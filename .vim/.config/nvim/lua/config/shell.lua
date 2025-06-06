fb39db58f1dfbb3fe783f05af85887cdf5d4fec8
SeanNeal572 <sean.neal@chillisoft.co.za>
2025-06-06 10:27:05 +0200

    Change default windows terminal to powershell

---
+++ .vim/.config/nvim/lua/config/shell.lua
@@ -0,0 +1,24 @@
+if vim.fn.has("win32") == 0 and vim.fn.has("win64") == 0 then
+  return
+end
+
+-- Check if 'pwsh' is executable and set the shell accordingly
+if vim.fn.executable("pwsh") == 1 then
+  vim.o.shell = "pwsh"
+else
+  vim.o.shell = "powershell"
+end
+
+-- Setting shell command flags
+vim.o.shellcmdflag =
+  "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
+
+-- Setting shell redirection
+vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
+
+-- Setting shell pipe
+vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
+
+-- Setting shell quote options
+vim.o.shellquote = ""
+vim.o.shellxquote = ""
