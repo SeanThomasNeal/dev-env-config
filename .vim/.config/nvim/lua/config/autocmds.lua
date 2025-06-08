4878a1191179254951e6b0ec10f7fffb9153d8a4
SeanNeal572 <sean.neal@chillisoft.co.za>
2025-06-08 17:31:45 +0200

    Set window title to project dir on opening a project

--- .vim/.config/nvim/lua/config/autocmds.lua
+++ .vim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("DirChanged", {
+  callback = function()
+    local cwd = vim.fn.getcwd()
+    local parts = vim.fn.split(cwd, "\\")
+    local projectFolder = vim.fn.get(parts, -1, "Error")
+
+    vim.opt.title = true
+    vim.opt.titlelen = 0
+    vim.opt.titlestring = projectFolder
+    vim.cmd("set title")
+  end,
+})
