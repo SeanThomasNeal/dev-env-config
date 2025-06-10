6877cab4f07ecfc504a2f33a189f825e5006e6ea
SeanNeal572 <sean.neal@chillisoft.co.za>
2025-06-10 11:41:27 +0200

    Unhide files in snacks picker

--- .vim/.config/nvim/lua/config/keymaps.lua
+++ .vim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+---@module 'snacks'
+
+LazyVim.safe_keymap_set("n", "<C-p>", function()
+  Snacks.picker.git_files()
+end, { desc = "Find file in git tree", remap = true })
