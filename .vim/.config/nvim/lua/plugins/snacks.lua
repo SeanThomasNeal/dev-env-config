14a609d21cb702a5b43a158d7b12ade7013774a5
SeanNeal572 <sean.neal@chillisoft.co.za>
2025-06-12 11:22:11 +0200

    Hide hidden and ignored files in grep

--- .vim/.config/nvim/lua/plugins/snacks.lua
+++ .vim/.config/nvim/lua/plugins/snacks.lua
@@ -2,7 +2,35 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
-      picker = { hidden = true, ignored = true, exclude = { ".git" } },
+      picker = {
+        hidden = true,
+        ignored = true,
+        exclude = { ".git" },
+        toggles = { hidden = false, ignored = false },
+      },
+    },
+    keys = {
+      {
+        "<C-p>",
+        function()
+          Snacks.picker.git_files()
+        end,
+        desc = "Find file in git tree",
+      },
+      {
+        "<leader>/",
+        function()
+          Snacks.picker.grep({ hidden = false, ignored = false })
+        end,
+        desc = "Grep",
+      },
+      {
+        "<leader>sg",
+        function()
+          Snacks.picker.grep({ hidden = false, ignored = false })
+        end,
+        desc = "Grep",
+      },
     },
   },
 }
