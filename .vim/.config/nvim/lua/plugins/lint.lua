71477405b1b91d93af83c2d31bfc720d671348e8
SeanNeal572 <sean.neal@chillisoft.co.za>
2025-06-20 08:59:09 +0200

    Add eslint linter and use biomejs conditionally

--- .vim/.config/nvim/lua/plugins/lint.lua
+++ .vim/.config/nvim/lua/plugins/lint.lua
@@ -1,12 +1,23 @@
+-- if true then
+--   return {}
+-- end
+
 return {
   {
     "mfussenegger/nvim-lint",
     opts = {
       linters_by_ft = {
-        ["javascript"] = { "biomejs" },
-        ["javascriptreact"] = { "biomejs" },
-        ["typescript"] = { "biomejs" },
-        ["typescriptreact"] = { "biomejs" },
+        ["javascript"] = { "biomejs", "eslint" },
+        ["javascriptreact"] = { "biomejs", "eslint" },
+        ["typescript"] = { "biomejs", "eslint" },
+        ["typescriptreact"] = { "biomejs", "eslint" },
+      },
+      linters = {
+        biomejs = {
+          condition = function(ctx)
+            return vim.fs.find({ "biome.json" }, { path = ctx.filename, upward = true })[1]
+          end,
+        },
       },
     },
   },
