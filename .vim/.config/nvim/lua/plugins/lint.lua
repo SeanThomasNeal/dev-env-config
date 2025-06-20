e27bc55dc3ecddf2c9fee22ffa96405bb469456e
SeanNeal572 <sean.neal@chillisoft.co.za>
2025-06-20 15:31:27 +0200

    Set condition for eslint

--- .vim/.config/nvim/lua/plugins/lint.lua
+++ .vim/.config/nvim/lua/plugins/lint.lua
@@ -18,6 +18,20 @@ return {
             return vim.fs.find({ "biome.json" }, { path = ctx.filename, upward = true })[1]
           end,
         },
+        eslint = {
+          condition = function(ctx)
+            local filenames = {
+              "eslint.config.js",
+              "eslint.config.mjs",
+              "eslint.config.cjs",
+              ".eslintrc.js",
+              ".eslintrc.cjs",
+              ".eslintrc.yaml",
+              ".eslint.json",
+            }
+            return vim.fs.find(filenames, { path = ctx.filename, upward = true })[1]
+          end,
+        },
       },
     },
   },
