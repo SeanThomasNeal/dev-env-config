e34c8d365d0fe607313be09989ff5003df472434
SeanNeal572 <sean.neal@chillisoft.co.za>
2025-07-14 13:57:04 +0200

    Fix prettier format not running

--- .vim/.config/nvim/lua/plugins/conform.lua
+++ .vim/.config/nvim/lua/plugins/conform.lua
@@ -4,12 +4,12 @@ return {
     opts = {
       formatters_by_ft = {
         lua = { "stylua" },
-        ["javascript"] = { "biome-check", prettier = "fallback" },
-        ["javascriptreact"] = { "biome-check", prettier = "fallback" },
-        ["typescript"] = { "biome-check", prettier = "fallback" },
-        ["typescriptreact"] = { "biome-check", prettier = "fallback" },
-        ["json"] = { "biome-check", prettier = "fallback" },
-        ["css"] = { "biome-check", prettier = "fallback" },
+        ["javascript"] = { "biome-check", "prettier", stop_after_first = true },
+        ["javascriptreact"] = { "biome-check", "prettier", stop_after_first = true },
+        ["typescript"] = { "biome-check", "prettier", stop_after_first = true },
+        ["typescriptreact"] = { "biome-check", "prettier", stop_after_first = true },
+        ["json"] = { "biome-check", "prettier", stop_after_first = true },
+        ["css"] = { "biome-check", "prettier", stop_after_first = true },
       },
     },
   },
