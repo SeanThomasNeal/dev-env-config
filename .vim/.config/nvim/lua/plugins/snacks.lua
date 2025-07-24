f8ee269fe0ec9ed487c6b8d46f089ee9dc880f10
SeanNeal572 <sean.neal@chillisoft.co.za>
2025-07-24 07:12:19 +0200

    Revert to using snacks toggles

--- .vim/.config/nvim/lua/plugins/snacks.lua
+++ .vim/.config/nvim/lua/plugins/snacks.lua
@@ -1,14 +1,6 @@
 return {
   {
     "folke/snacks.nvim",
-    opts = {
-      picker = {
-        hidden = true,
-        ignored = true,
-        exclude = { ".git" },
-        toggles = { hidden = false, ignored = false },
-      },
-    },
     keys = {
       {
         "<C-p>",
@@ -16,20 +8,6 @@ return {
           Snacks.picker.git_files()
         end,
         desc = "Find file in git tree",
-      },
-      {
-        "<leader>/",
-        function()
-          Snacks.picker.grep({ hidden = false, ignored = false })
-        end,
-        desc = "Grep",
-      },
-      {
-        "<leader>sg",
-        function()
-          Snacks.picker.grep({ hidden = false, ignored = false })
-        end,
-        desc = "Grep",
       },
     },
   },
