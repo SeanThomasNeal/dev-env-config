e7c521fc8e599c7108bb012c10f34350b79c1741
SeanThomasNeal <seanneal572@gmail.com>
2025-08-10 13:01:48 +0200

    Updated windows init

--- windows/init.ps1
+++ windows/init.ps1
@@ -1,8 +1,8 @@
-$SETUP_DIR="$DEV_ENV_CONFIG_DIR\setup"
+$SETUP_DIR="$DEV_ENV_CONFIG_DIR\windows\setup"
 
 . "$SETUP_DIR\vim.ps1"
 
-$ALIAS_DIR="$DEV_ENV_CONFIG_DIR\aliases"
+$ALIAS_DIR="$DEV_ENV_CONFIG_DIR\windows\aliases"
 
 . "$ALIAS_DIR\git.ps1"
 . "$ALIAS_DIR\vim.ps1"
