224a62b30034f45a4aa1b3f1fa794f411233c4c5
SeanThomasNeal <seanneal572@gmail.com>
2025-08-16 22:59:16 +0200

    Creating local lua file for workspace plugin

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-vim.fn.setenv("foobar", vim.fn.expand("%"))
-
 require("config.lazy")
 require("config.shell")
--- config/nvim/lua/plugins/custom/init.lua
+++ config/nvim/lua/plugins/custom/init.lua
@@ -1,5 +1,6 @@
 local info = debug.getinfo(1, "S")
 local currentFolder = info.source:sub(2):match("(.*/)") or nil
+vim.fn.setenv("custom-plugins-dir", currentFolder)
 
 if currentFolder == nil then
   return {}
