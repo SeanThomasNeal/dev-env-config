e25366c9682726555c4431020053ca8f610e04b3
SeanThomasNeal <seanneal572@gmail.com>
2025-08-16 15:54:41 +0200

    Setup first custom plugin

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+vim.fn.setenv("foobar", vim.fn.expand("%"))
+
 require("config.lazy")
 require("config.shell")
---
+++ config/nvim/lua/plugins/custom/init.lua
@@ -0,0 +1,15 @@
+local info = debug.getinfo(1, "S")
+local currentFolder = info.source:sub(2):match("(.*/)") or nil
+
+if currentFolder == nil then
+  return {}
+end
+
+return {
+  {
+    dir = currentFolder .. "workspace.nvim",
+    config = function()
+      require("workspace")
+    end,
+  },
+}
