f99d087485ff81796bbf8f27da203f536d7bad98
SeanThomasNeal <seanneal572@gmail.com>
2025-08-17 21:26:18 +0200

    Added setting of workspace values

--- config/nvim/lua/plugins/custom/util.nvim/lua/util.lua
+++ config/nvim/lua/plugins/custom/util.nvim/lua/util.lua
@@ -4,8 +4,6 @@ local configFile = currentFolder .. "config.json"
 
 local config = vim.empty_dict()
 
-local M = {}
-
 local function saveConfig()
   vim.fn.writefile({ vim.json.encode(config) }, configFile)
 end
@@ -14,6 +12,13 @@ local function loadConfig()
   local lines = vim.fn.readfile(configFile)
   config = vim.json.decode(vim.fn.join(lines))
 end
+
+local function setConfig(configName, value)
+  config[configName] = value
+  saveConfig()
+end
+
+local M = {}
 
 function M.setup()
   if vim.fn.filereadable(configFile) == 1 then
@@ -29,19 +34,10 @@ function M.getConfig(configName)
   }
 
   function R.save()
-    M.setConfig(configName, R.config)
+    setConfig(configName, R.value)
   end
 
   return R
-end
-
-function M.hasConfig(configName)
-  return vim.fn.has_key(config, configName)
-end
-
-function M.setConfig(configName, value)
-  config[configName] = value
-  saveConfig()
 end
 
 return M
--- config/nvim/lua/plugins/custom/workspace.nvim/lua/workspace.lua
+++ config/nvim/lua/plugins/custom/workspace.nvim/lua/workspace.lua
@@ -20,4 +20,39 @@ vim.api.nvim_create_user_command("ListWorkspaces", function()
   vim.notify(vim.fn.join(vim.fn.keys(config.value), " - "), 0)
 end, { nargs = 0 })
 
+vim.api.nvim_create_user_command("SetWorkspaceTitle", function(opts)
+  local workspaceKey = opts.fargs[1]
+  local workspaceTitle = opts.fargs[2]
+
+  config.value[workspaceKey].title = workspaceTitle
+  config.save()
+end, { nargs = "*" })
+
+vim.api.nvim_create_user_command("SetWorkspacePath", function(opts)
+  local workspaceKey = opts.fargs[1]
+  local workspacePath = opts.fargs[2]
+
+  config.value[workspaceKey].path = workspacePath
+  config.save()
+end, { nargs = "*" })
+
+vim.api.nvim_create_user_command("SetWorkspaceName", function(opts)
+  local workspaceKey = opts.fargs[1]
+  local workspaceName = opts.fargs[2]
+
+  config.value[workspaceKey].name = workspaceName
+  config.save()
+end, { nargs = "*" })
+
+vim.api.nvim_create_user_command("OpenWorkspace", function(opts)
+  local workspaceKey = opts.fargs[1]
+  local workspace = config.value[workspaceKey]
+
+  vim.fn.chdir(workspace.path)
+  vim.opt.title = true
+  vim.opt.titlelen = 0
+  vim.opt.titlestring = workspace.title
+  vim.cmd("set title")
+end, { nargs = 1 })
+
 return M
