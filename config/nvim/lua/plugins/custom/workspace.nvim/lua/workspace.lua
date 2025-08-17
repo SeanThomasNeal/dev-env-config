337fcee5eb625a851efd730babbe07b6b3faba00
SeanThomasNeal <seanneal572@gmail.com>
2025-08-17 19:50:57 +0200

    Remove constants module

--- config/nvim/lua/plugins/custom/workspace.nvim/lua/workspace.lua
+++ config/nvim/lua/plugins/custom/workspace.nvim/lua/workspace.lua
@@ -1,11 +1,10 @@
 local utils = require("util")
-local constants = require("constants")
 
 local M = {}
 
 function M.setup()
-  if utils.hasConfig(constants.configName) == 0 then
-    utils.setConfig(constants.configName, vim.empty_dict())
+  if utils.hasConfig("workspaces") == 0 then
+    utils.setConfig("workspaces", vim.empty_dict())
   end
 end
 
