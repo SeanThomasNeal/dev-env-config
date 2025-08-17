28d70835301fc92f42174f875117d5cc41638c89
SeanThomasNeal <seanneal572@gmail.com>
2025-08-17 14:57:59 +0200

    Refactored config utils

--- config/nvim/lua/plugins/custom/util.nvim/lua/util.lua
+++ config/nvim/lua/plugins/custom/util.nvim/lua/util.lua
@@ -6,18 +6,34 @@ local config = vim.empty_dict()
 
 local M = {}
 
+local function saveConfig()
+  vim.fn.writefile({ vim.json.encode(config) }, configFile)
+end
+
+local function loadConfig()
+  local lines = vim.fn.readfile(configFile)
+  config = vim.json.decode(vim.fn.join(lines))
+end
+
 function M.setup()
-  if vim.fn.filereadable(configFile) == 0 then
-    vim.fn.writefile({ vim.json.encode(config) }, configFile, "a")
+  if vim.fn.filereadable(configFile) == 1 then
+    loadConfig()
   else
-    local lines = vim.fn.readfile(configFile)
-    local undecoded = vim.fn.join(lines)
-    config = vim.json.decode(undecoded)
+    saveConfig()
   end
 end
 
 function M.getConfig(configName)
   return vim.fn.get(config, configName)
+end
+
+function M.hasConfig(configName)
+  return vim.fn.has_key(config, configName)
+end
+
+function M.setConfig(configName, value)
+  config[configName] = value
+  saveConfig()
 end
 
 return M
