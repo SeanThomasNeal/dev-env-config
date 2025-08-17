1dc727ea8261ffdb73020327116823d4981039ab
SeanThomasNeal <seanneal572@gmail.com>
2025-08-17 20:20:52 +0200

    Create AddWorkspace command

--- config/nvim/lua/plugins/custom/workspace.nvim/lua/workspace.lua
+++ config/nvim/lua/plugins/custom/workspace.nvim/lua/workspace.lua
@@ -1,11 +1,24 @@
 local utils = require("util")
 
+local workspacesConfig = vim.empty_dict()
+
+local function saveConfig()
+  utils.setConfig("workspaces", workspacesConfig)
+end
+
 local M = {}
 
 function M.setup()
   if utils.hasConfig("workspaces") == 0 then
-    utils.setConfig("workspaces", vim.empty_dict())
+    utils.setConfig("workspaces", workspacesConfig)
+  else
+    workspacesConfig = utils.getConfig("workspaces")
   end
 end
+
+vim.api.nvim_create_user_command("AddWorkspace", function(opts)
+  workspacesConfig[opts.fargs[1]] = vim.empty_dict()
+  saveConfig()
+end, { nargs = 1 })
 
 return M
