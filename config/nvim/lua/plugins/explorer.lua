fc3df894db75707d0cb389bb36827c189170d972
SeanNeal572 <sean.neal@chillisoft.co.za>
2025-08-21 14:26:48 +0200

    Remap explorer open keybinds

---
+++ config/nvim/lua/plugins/explorer.lua
@@ -0,0 +1,22 @@
+return {
+  "folke/snacks.nvim",
+  keys = {
+    {
+      "<leader>fE",
+      function()
+        ---@diagnostic disable-next-line: missing-fields
+        Snacks.explorer({ cwd = LazyVim.root() })
+      end,
+      desc = "Explorer Snacks (root dir)",
+    },
+    {
+      "<leader>fe",
+      function()
+        Snacks.explorer()
+      end,
+      desc = "Explorer Snacks (cwd)",
+    },
+    { "<leader>E", "<leader>fE", desc = "Explorer Snacks (root dir)", remap = true },
+    { "<leader>e", "<leader>fe", desc = "Explorer Snacks (cwd)", remap = true },
+  },
+}
