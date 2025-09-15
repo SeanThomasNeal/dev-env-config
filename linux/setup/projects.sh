7933353370ee191dee9434af56a9d076f957368d
SeanNeal572 <sean.neal@chillisoft.co.za>
2025-09-15 15:09:46 +0200

    Updated default for project json

--- linux/setup/projects.sh
+++ linux/setup/projects.sh
@@ -1,4 +1,11 @@
 export PROJECTS_JSON="$DEV_ENV_CONFIG_DIR/projects.json"
 if [ ! -f "$PROJECTS_JSON" ]; then
-  echo "{}" >>"$PROJECTS_JSON"
+  (
+    echo "{"
+    echo -e "\t\"config\": {"
+    echo -e "\t\t\"dir\": \"$DEV_ENV_CONFIG_DIR\""
+    echo -e "\t\t\"title\": \"dev-env-config\""
+    echo -e "\t}"
+    echo "}"
+  ) >>"$PROJECTS_JSON"
 fi
